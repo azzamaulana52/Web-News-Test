f26c8a7a82d5d8e31f1dfd02e2d3fc27db807b5f
azzamaulana52 <azzamaulana52@gmail.com>
2025-11-08 00:15:11 +0700

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,15 +1,3 @@
 export default async function handler(req, res) {
   const apiKey = process.env.NEWS_API_KEY; // simpan API key di environment variable
-  const url = `https://newsapi.org/v2/top-headlines?country=id&apiKey=${apiKey}`;
-
-  try {
-    const response = await fetch(`https://newsapi.org/v2/everything?q=technology&apiKey=${apiKey}`);
-    if (!response.ok) {
-      return res.status(response.status).json({ message: 'Error fetching news from NewsAPI' });
-    }
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-}
+  const url = `https://newsapi.org/v2/everything?q=technology&apiKey=${apiKey}`;
